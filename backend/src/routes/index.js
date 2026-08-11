5c10508abaa0d20802bf47e4685273ea8ad70014
Carlos Junior <carloslaujunior23@gmail.com>
2026-08-11 20:35:57 -0300

    feat: adiciona middleware de autenticacao com JWT

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
 
 const userController = require('../controllers/userController');
+const auth = require('../middlewares/auth');
 
 const routes = Router();
 
 routes.post('/usuarios', userController.cadastrar);
 routes.post('/login', userController.login);
+
+routes.use(auth);
+
+routes.get('/teste', (req, res) => {
+  return res.json({ mensagem: 'Token valido', usuarioId: req.usuarioId });
+});
 
 module.exports = routes;
